38997d3a408ad74002c39a9938bea384e868aaa6
Kevin D Smith <kevin.smith@sas.com>
2020-12-18 09:30:03 -0500

    Remove junit

--- conda.recipe/test_package.R
+++ conda.recipe/test_package.R
@@ -3,5 +3,5 @@ junit_xml <- paste(Sys.getenv("TEST_REPORT_DIR"),
                    "-junit.xml", sep="")
 test_dir <- system.file("tests", "testthat", package = "swat")
 
-testthat::test_dir(test_dir, stop_on_failure=TRUE,
-                   reporter=testthat::JunitReporter$new(file=junit_xml))
+testthat::test_dir(test_dir, stop_on_failure=TRUE) #,
+                  #reporter=testthat::JunitReporter$new(file=junit_xml))
